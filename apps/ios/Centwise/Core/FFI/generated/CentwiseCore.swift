5a2341cdad910d50f3fb907ab5912362f51e0e28
NAHIDBrOnZ007 <freefire00940@gmail.com>
2026-08-27 20:39:54 +0600

    github action failed fix 1

--- apps/ios/Centwise/Core/FFI/generated/CentwiseCore.swift
+++ apps/ios/Centwise/Core/FFI/generated/CentwiseCore.swift
@@ -7,9 +7,7 @@ import Foundation
 // Depending on the consumer's build setup, the low-level FFI code
 // might be in a separate module, or it might be compiled inline into
 // this module. This is a bit of light hackery to work with both.
-#if canImport(CentwiseCoreFFI)
 import CentwiseCoreFFI
-#endif
 
 fileprivate extension RustBuffer {
     // Allocate a new buffer, copying the contents of a `UInt8` array.
